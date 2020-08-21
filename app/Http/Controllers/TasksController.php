a7167d2905ada648292277b67abcf5da31ff54b9
hiroshi.yokota <dummy@gmail.com>
2020-08-21 07:15:45 +0000

    add login

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -8,17 +8,35 @@ use App\Task;    // 追加
 
 class TasksController extends Controller
 {
-    // getでtasks/にアクセスされた場合の「一覧表示処理」
     public function index()
     {
+        $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得
+            $tasks = Task::where('user_id', $user->id)->get();
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+        }
+
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
+    }
+    // getでtasks/にアクセスされた場合の「一覧表示処理」
+//    public function index()
+//    {
         // メッセージ一覧を取得
-        $tasks = Task::all();
+//        $tasks = Task::all();
 
         // メッセージ一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
-    }
+//        return view('tasks.index', [
+//            'tasks' => $tasks,
+//        ]);
+//    }
+
 
     /**
      * Show the form for creating a new resource.
@@ -50,10 +68,12 @@ class TasksController extends Controller
             'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
+        $user = \Auth::user();
         // メッセージを作成
         $task = new Task;
         $task->status = $request->status;    // 追加
         $task->content = $request->content;
+        $task->user_id = $user->id;    // 追加
         $task->save();
 
         // トップページへリダイレクトさせる
